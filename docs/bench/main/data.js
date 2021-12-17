c41e7527e741794277ff2177aba6a64deb2be5b4
github-action-benchmark <github@users.noreply.github.com>
2021-12-17 21:57:18 +0000

    add Benchmark (cargo) benchmark result for 80ced300c91550d640f9e3aa25fb911281100a49

--- docs/bench/main/data.js
+++ docs/bench/main/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639776785646,
+  "lastUpdate": 1639778238099,
   "repoUrl": "https://github.com/jmfiaschi/json_value_search",
   "entries": {
     "Benchmark": [
@@ -53,6 +53,60 @@ window.BENCHMARK_DATA = {
             "name": "search//field/*/regex",
             "value": 76921,
             "range": "± 584",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "jm.fiaschi",
+            "username": "jmfiaschi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80ced300c91550d640f9e3aa25fb911281100a49",
+          "message": "fix(cicd): automatize cargo version update (#1)",
+          "timestamp": "2021-12-17T22:53:50+01:00",
+          "tree_id": "f46bfbfa3b9cd6d2bb527cea445cf4fdb2d6cc2b",
+          "url": "https://github.com/jmfiaschi/json_value_search/commit/80ced300c91550d640f9e3aa25fb911281100a49"
+        },
+        "date": 1639778237116,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "search//field/other_field",
+            "value": 29577,
+            "range": "± 1742",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "search//field/*/other_field).",
+            "value": 58012,
+            "range": "± 3563",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "search//field/1/other_field).",
+            "value": 30835,
+            "range": "± 1522",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "search//field/1",
+            "value": 18740,
+            "range": "± 783",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "search//field/*/regex",
+            "value": 95857,
+            "range": "± 3962",
             "unit": "ns/iter"
           }
         ]
